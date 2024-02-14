f779f52d3b0722791454669d1bf12eb5bdc65a07
Mehran Khodaei <khodaei.dev@gmail.com>
2024-02-15 00:01:59 +0330

    install &  Required packages in app.js

---
+++ back-end/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const authRoter = require("./routes/auth");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/auth/", authRoter);
+
+module.exports = app;
