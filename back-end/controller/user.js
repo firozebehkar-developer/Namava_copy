1fe72e44641762378470dc7c9a9ba77a8dac427d
Mehran Khodaei <khodaei.dev@gmail.com>
2024-02-16 23:19:09 +0330

    Fix Big For Valid ObjectId

--- back-end/controller/user.js
+++ back-end/controller/user.js
@@ -1,8 +1,15 @@
 const userModel = require("./../models/user");
 const bsanUserModel = require("./../models//ban-phone");
+const { isValidObjectId } = require("mongoose");
 
 exports.banUser = async (req, res) => {
-  const { id } = req.params.id;
+  const id = req.params.id;
+  const validId = isValidObjectId(id);
+
+  if (!validId) {
+    return res.status(400).json({ message: "ObjectId is Not valid" });
+  }
+
   const findUser = await userModel.findOne({ _id: id });
   const banUser = await bsanUserModel.create({ phone: findUser.phone });
 
