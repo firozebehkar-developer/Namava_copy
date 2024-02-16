cf72b8d6d41bd2b744ff16dc9f079afb832f243c
Mehran Khodaei <khodaei.dev@gmail.com>
2024-02-16 22:56:01 +0330

    MiddlewarD development to limit access to api only bAdmins

---
+++ back-end/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("./../models/user");
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.header("Authorization")?.split(" ");
+  if (authHeader?.length !== 2) {
+    return res.status(403).json({
+      message: "This api protected",
+    });
+  }
+
+  const token = authHeader[1];
+
+  try {
+    const verifyToken = await jwt.verify(token, process.env.jwt_secret);
+    const user = await userModel.findById(verifyToken.id).lean();
+
+    Reflect.deleteProperty(user, "password");
+
+    req.user = user;
+    return next();
+  } catch (err) {
+    res.json(err);
+  }
+};
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const userController = require("./../controller/user");
 const router = express.Router();
+const authMiddleware = require("./../middleware/auth");
+const isAdminMiddleware = require("./../middleware/isAdmin");
 
-router.post("/ban/:id", userController.banUser);
+router
+  .route("/ban/:id")
+  .post(authMiddleware, isAdminMiddleware, userController.banUser);
 
 module.exports = router;
