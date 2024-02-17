1d3b7ab79b21f27613b98f4a0e442321e44c8458
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-17 07:27:52 +0330

    update app.js
    add custom elements site-navbar & slider-header

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -1,5 +1,7 @@
 import { Navbar } from "../components/Navbar/Navbar.js";
+import { Slider_header } from "../components/Slider-header/Slider-header.js";
 
-window.customElements.define('site-navbar',Navbar)
+window.customElements.define('site-navbar',Navbar);
+window.customElements.define('slider-header',Slider_header);
 
 
