000cea0533c102ecf6544f31dac948c07dc4d6ef
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-17 22:50:58 +0330

    fix bug

--- front-end/src/js/NamavaMostVisited.js
+++ front-end/src/js/NamavaMostVisited.js
@@ -1,8 +1,7 @@
 "use strict";
 
-// import { Navbar } from "../components/Navbar/Navbar.js";
-
-// window.customElements.define("site-navbar", Navbar);
+import {Navbar}  from "../components/Navbar/Navbar.js";
+window.customElements.define("site-navbar", Navbar);
 
 const MainContainer = document.querySelector(".main__container");
 
