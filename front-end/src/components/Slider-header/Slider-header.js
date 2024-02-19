4c1e9e631a3741ac3e9e47c8459911d687b4f21b
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-19 05:02:47 +0330

    tanha
    add slider 2

--- front-end/src/components/Slider-header/Slider-header.js
+++ front-end/src/components/Slider-header/Slider-header.js
@@ -14,7 +14,8 @@ template.innerHTML = `
             <p>زیرنویس
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
               </svg>
             </p>
           </li>
@@ -51,13 +52,14 @@ template.innerHTML = `
             <p>+15</p>
           </li>
         </ul>
-  
+
       </div>
-  
-      <p class="text-p div-text">یک معلم تاریخ بداخلاق که در یک دانشگاه شبانه روزی دورافتاده تدریس می کند، مجبور می شود در
+
+      <p class="text-p div-text">یک معلم تاریخ بداخلاق که در یک دانشگاه شبانه روزی دورافتاده تدریس می کند، مجبور می
+        شود در
         حین تعطیلات کریسمس، کنار دانش آموزی که جایی برای رفتن ندارد بماند
       </p>
-  
+
       <p class="text-p">هم اکنون با دوبله اختصاصی نماوا</p>
       <div class="div-btna">
         <a class="link-desc" href="#">
@@ -70,7 +72,74 @@ template.innerHTML = `
       <p class="stars">Paul GiamattiDa'Vine Joy RandolphDominic Sessa : ستارگان</p>
     </div>
   </div>
-  <div class="swiper-slide div-2">Slide 2</div>
+  <div class="swiper-slide div-2">
+    <div class="div-cont">
+      <img class="poster" src="./src/assets/imgs/slider-header/img2.png" alt="">
+      <h1 class="text-p">سلاطین گل کوچیک: از خیابان تا جام جهانی</h1>
+      <div class="div-data">
+        <ul>
+          <li>
+            <p>زیرنویس
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+              </svg>
+            </p>
+          </li>
+          <li>
+            <p> دوبله نماوا
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+              </svg>
+            </p>
+          </li>
+          <li>
+            <p>84%
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+              </svg>
+            </p>
+          </li>
+          <li>
+            <p>8</p>
+          </li>
+          <li>
+            <p>
+              <span>69 </span> دقیقه
+            </p>
+          </li>
+          <li>
+            <p>1402</p>
+          </li>
+          <li>
+            <p>+12</p>
+          </li>
+        </ul>
+
+      </div>
+
+      <p class="text-p div-text">
+        سلاطین گل کوچیک: از خیابان تا جام جهانی، فیلم مستندی است که با مصاحبه با صاحب نظران ورزش فوتبال، چـگونـگـی
+        پـیدایـش، رواج و سـرانـجام کـمرنـگ شـدن فوتبال خیابانی را بـه تـصویـر می کشد.
+      </p>
+
+      <p class="text-p">هم اکنون با دوبله اختصاصی نماوا</p>
+      <div class="div-btna">
+        <a class="link-desc" href="#">
+          توضیحات بیشتر
+        </a>
+        <a class="link-ashtrak" href="#">
+          خرید اشتراک
+        </a>
+      </div>
+      <p class="stars">ستارگان:وحید شمسایی و حسین شمس و سید مهدی ابطحی</p>
+    </div>
+  </div>
   <div class="swiper-slide div-3">Slide 3</div>
   <div class="swiper-slide div-4">Slide 4</div>
   <div class="swiper-slide div-5">Slide 5</div>
