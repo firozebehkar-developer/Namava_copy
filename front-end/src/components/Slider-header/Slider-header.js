885628951b08d47a85a29210177cfe14ba410314
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-19 05:28:35 +0330

    tanha
    add slider 4

--- front-end/src/components/Slider-header/Slider-header.js
+++ front-end/src/components/Slider-header/Slider-header.js
@@ -207,7 +207,74 @@ template.innerHTML = `
       <p class="stars">ستارگان : natascha mcelhone ,Yeainha , poloscheriber</p>
     </div>
   </div>
-  <div class="swiper-slide div-4">Slide 4</div>
+  <div class="swiper-slide div-4">
+        <video autoplay loop muted src="./src/assets/imgs/slider-header/bgdiv-4.mp4"></video>
+        <div class="div-cont">
+          <img class="poster" src="./src/assets/imgs/slider-header/img4.png" alt="">
+          <h1 class="text-p">آبجی</h1>
+          <div class="div-data">
+            <ul>
+              <li>
+                <p>زیرنویس
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                  </svg>
+                </p>
+              </li>
+              <li>
+                <p> دوبله نماوا
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+                  </svg>
+                </p>
+              </li>
+              <li>
+                <p>88%
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                  </svg>
+                </p>
+              </li>
+              <li>
+                <p>6.3</p>
+              </li>
+              <li>
+                <p>
+                  <span>99 </span> دقیقه
+                </p>
+              </li>
+              <li>
+                <p>1394</p>
+              </li>
+              <li>
+                <p>+12</p>
+              </li>
+            </ul>
+
+          </div>
+
+          <p class="text-p div-text">
+            طلا و دختر معلولش عطی، سالیان سال است که با یکدیگر زندگی می‌کنند. پس از آنکه طلا متوجه تشدید بیماری‌ خود و احتمال مرگش در آینده نزدیک می‌شود، تصمیم می‌گیرد برای تامین امنیت آینده عطی، اقدام کند و...
+          </p>
+
+          <p class="text-p">هم اکنون با دوبله اختصاصی نماوا</p>
+          <div class="div-btna">
+            <a class="link-desc" href="#">
+              توضیحات بیشتر
+            </a>
+            <a class="link-ashtrak" href="#">
+              خرید اشتراک
+            </a>
+          </div>
+          <p class="stars">ستارگان:گلاب آدینهپانته آ پناهی هاعلیرضا استادی</p>
+        </div>
+      </div>
   <div class="swiper-slide div-5">Slide 5</div>
   <div class="swiper-slide div-6">Slide 6</div>
   <div class="swiper-slide div-7">Slide 7</div>
