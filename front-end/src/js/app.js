33597f84517e3a5d2d8f5d0e0f5e5b533f7d32dc
ّFRONT-END DEVELOPER <145340995+Abolfazl-mollashahi@users.noreply.github.com>
2024-02-20 04:01:45 +0330

    tanha
    add 2 line footer in app.js

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -1,7 +1,9 @@
 import { Navbar } from "../components/Navbar/Navbar.js";
+import { Footer } from "../components/Footer/Footer.js";
 import { Slider_header } from "../components/Slider-header/Slider-header.js";
 
 window.customElements.define('site-navbar',Navbar);
 window.customElements.define('slider-header',Slider_header);
+window.customElements.define('site-footer',Footer);
 
 
